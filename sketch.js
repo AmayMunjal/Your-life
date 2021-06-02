dc4c598c817b91fc32173bd6ff61372214bab589
AmayMunjal <74940949+AmayMunjal@users.noreply.github.com>
2021-06-01 18:54:38 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 var gameState = 0
 var bg1 
 var playerStanding, playerRunning1,playerRunning2, player 
+var car,carImg
+var invG1,invG2
 
 function preload(){
   bg1 = loadImage("playerhouse.png")
@@ -13,7 +15,8 @@ function setup(){
  // var bg1 = createSprite(200,200,400,400)
  var player = createSprite(740,555,10,10)
  player.addImage(playerStanding)
- player.visible = false
+ invG1 = createSprite(735,670,1500,10)
+ //player.visible = false
 }
 
 
@@ -27,14 +30,28 @@ function draw() {
     text("Make Your Own Decisions  ",600,220)
     text("But Be Careful What You Choose.",575,250)
     text("Press 'Space' To Continue",600,400)
+   
     if (keyDown("space")){
       gameState = 1
     }
   }
   if (gameState === 1){
     background(bg1)
-    player.visible = true
+    //player.visible = true
     text(mouseX + "," + mouseY,mouseX,mouseY)
+    console.log("0")
+    if (keyDown("W")){
+     // player.velocityY = -3
+    }
+    //player.velocityY = player.velocityY +0.5
+    if (keyDown("A")){
+      player.x = player.x -5
+    }
+    if (keyDown("D")){
+      player.x = player.x +5
+    }
+    player.collide(invG1)
+    drawSprites()
   }
-  drawSprites()
+ 
 }
